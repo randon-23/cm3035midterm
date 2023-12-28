591def4d072c19086576d4151e744462fd6ef904
randon-23 <arandon24@gmail.com>
2023-12-29 00:09:34 +0100

    SET STATIC ROOT

--- cm3035midterm/settings.py
+++ cm3035midterm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +81,7 @@ WSGI_APPLICATION = 'cm3035midterm.wsgi.application'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'superstore' / 'static']
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
